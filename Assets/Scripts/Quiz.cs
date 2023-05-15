3208d635dc56b4f734073cc36da789a7b50d33b4
Gumbercules35 <mattwalden@gmail.com>
2023-05-15 13:33:36 +0100

    iteration of answer buttons to fill answer text

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,9 +7,16 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI questionText;
    [SerializeField] private QuestionSO question;
+   [SerializeField] private GameObject[] answerButtons;
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+        TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = question.GetAnswer(i);            
+        }
     }
 
 }
